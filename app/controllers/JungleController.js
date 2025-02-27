dce39bdb0f6e6af5d7684a569377eb4e721192d6
paul <stittpaul2001@gmail.com>
2025-02-27 16:24:54 -0700

    added drawjumble list

--- app/controllers/JungleController.js
+++ app/controllers/JungleController.js
@@ -1,1 +1,16 @@
 import { AppState } from "../AppState.js"
+
+
+export class JungleController {
+    constructor(){
+        this.drawjumblelist()
+    }
+    drawjumblelist() {
+        const jumbleFiles = AppState.jumbleFiles
+        let jumbleFilesContent = ''
+        jumbleFiles.forEach(jumbleFile => jumbleFilesContent +=
+        jumbleFile.listHTMLTemplate)
+        const jumbleFilesListElem = document.getElementById('JumbleList')
+        jumbleFilesListElem.innerHTML = jumbleFilesContent
+    }
+}
