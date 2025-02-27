de9a10632daa32d07053f5d5a9eae34f7a715d8c
Ivan <mayhemk57@gmail.com>
2025-02-27 15:43:22 -0700

    array set of jumbles

---
+++ app/AppState.js
@@ -0,0 +1,9 @@
+/** @type {import('./models/Jungle.js').Jumble[]} */
+
+import { Jumble } from './models/Jungle.js';
+
+jumbles = [
+  new Jumble({ name: '🐒 Jumble', body: "Monkeys go bananas for fruit salad, especially when it's filled with juicy pineapples." }),
+  new Jumble({ name: '🦍 Jumble', body: "The gorilla juggled berries and grapes, adding them to the fruit salad one-by-one. As nimble as a spider monkey, the typist skillfully typed sentence after sentence." }),
+  new Jumble({ name: '🦀 Jumble', body: "Oranges, apples, and bananas danced together in the bowl, creating a harmonious fruit salad. With each keystroke, the orangutan got closer to becoming a typing virtuoso. Hungry for success? Keep peeling away at those keys until you're the fastest typist in the jungle!" })
+]
---
+++ app/models/Jungle.js
@@ -0,0 +1,17 @@
+export class Jumble {
+  constructor(data) {
+    // this.id = generateId()
+    this.name = data.name
+    this.body = data.body
+
+    this.fastestTime = null
+    this.startTime = null
+    this.endTime = null
+  }
+
+  get ListTemplate() {
+    return `<li>${this.name}</li>`
+  }
+
+
+}
